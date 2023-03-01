f96b5237b38352e21c08526561765d2d4a4cdd41
Pascal van Leeuwen <pascal@pokt.network>
2023-02-28 18:21:23 -0800

    feat: updated types package to match new DB ERD

---
+++ types/portal_app.go
@@ -0,0 +1,255 @@
+package types
+
+import (
+	"sort"
+	"time"
+)
+
+/* Enums */
+type (
+	Environment string
+
+	NotificationType  string
+	NotificationEvent string
+
+	Origin    string
+	UserAgent string
+	Method    string
+	Contract  string
+
+	AppWhitelistType   string
+	ChainWhitelistType string
+)
+
+const (
+	EnvProduction Environment = "production"
+	EnvTest       Environment = "test"
+
+	NotificationEmail   NotificationType = "email"
+	NotificationWebhook NotificationType = "webhook"
+	NotificationPortal  NotificationType = "portal"
+
+	EventSignedUp      NotificationEvent = "signedUp"
+	EventQuarter       NotificationEvent = "quarter"
+	EventHalf          NotificationEvent = "half"
+	EventThreeQuarters NotificationEvent = "threeQuarters"
+	EventFull          NotificationEvent = "full"
+
+	WLOrigins     AppWhitelistType = "origins"
+	WLBlockchains AppWhitelistType = "blockchains"
+	WLUserAgents  AppWhitelistType = "userAgents"
+
+	WLMethods   ChainWhitelistType = "methods"
+	WLContracts ChainWhitelistType = "contracts"
+)
+
+/* PortalApp Struct Definition and Methods */
+type (
+	// PortalApp represents a single application in the Portal
+	PortalApp struct {
+		ID            string                               `json:"id"`
+		Name          string                               `json:"name"`
+		Gigastake     bool                                 `json:"gigastake"`
+		Account       Account                              `json:"account"`
+		AAT           AppAAT                               `json:"aat"`
+		Settings      AppSettings                          `json:"settings"`
+		Whitelists    AppWhitelists                        `json:"whitelists"`
+		Notifications map[NotificationType]AppNotification `json:"notifications"`
+		CreatedAt     time.Time                            `json:"createdAt"`
+		UpdatedAt     time.Time                            `json:"updatedAt"`
+	}
+
+	AppAAT struct {
+		AppID           ApplicationID `json:"appID,omitempty"`
+		Address         string        `json:"address"`
+		PublicKey       string        `json:"publicKey"`
+		ClientPublicKey string        `json:"clientPublicKey"`
+		PrivateKey      string        `json:"privateKey"`
+		Signature       string        `json:"signature"`
+		Version         string        `json:"version"`
+	}
+
+	AppSettings struct {
+		AppID                  ApplicationID             `json:"appID,omitempty"`
+		Environment            Environment               `json:"environment"`
+		SecretKey              string                    `json:"secretKey"`
+		SecretKeyRequired      bool                      `json:"secretKeyRequired"`
+		FavoritedBlockchainIDs map[BlockchainID]struct{} `json:"favoritedBlockchainIDs"`
+		// MonthlyRelayLimit sets the monthly limit per-application
+		// Sum of an Account's Apps MonthlyRelayLimits cannot exceed the Account's MonthlyRelayLimit
+		MonthlyRelayLimit int `json:"monthlyRelayLimit"`
+	}
+
+	AppWhitelists struct {
+		AppID       ApplicationID                          `json:"appID,omitempty"`
+		Origins     map[Origin]struct{}                    `json:"origins"`
+		UserAgents  map[UserAgent]struct{}                 `json:"userAgents"`
+		Blockchains map[BlockchainID]struct{}              `json:"blockchains"`
+		Contracts   map[BlockchainID]map[Contract]struct{} `json:"contracts"`
+		Methods     map[BlockchainID]map[Method]struct{}   `json:"methods"`
+	}
+
+	AppNotification struct {
+		AppID       string                     `json:"appID,omitempty"`
+		Active      bool                       `json:"active"`
+		Destination string                     `json:"destination"`
+		Trigger     string                     `json:"trigger"`
+		Events      map[NotificationEvent]bool `json:"events"`
+	}
+
+	// WhitelistsObject is a GraphQL-compatible representation of all the whitelists for a given application (used for the Portal UI)
+	WhitelistsObject struct {
+		AppWhitelists   [3]ApplicationWhitelists `json:"appWhitelists"`
+		ChainWhitelists [2]ChainWhitelists       `json:"chainWhitelists"`
+	}
+	ApplicationWhitelists struct {
+		Type   AppWhitelistType `json:"type"`
+		Values []string         `json:"values"`
+	}
+	ChainWhitelists struct {
+		Type   ChainWhitelistType       `json:"type"`
+		Values []BlockchainIDWhitelists `json:"values"`
+	}
+	BlockchainIDWhitelists struct {
+		BlockchainID string   `json:"blockchainID"`
+		Values       []string `json:"values"`
+	}
+
+	//UpdatePortalApp Struct Definition and Methods
+	UpdatePortalApp struct {
+		ApplicationID        ApplicationID
+		Name                 *string                              `json:"name,omitempty"`
+		GatewaySettings      *UpdateApplicationSettings           `json:"gatewaySettings,omitempty"`
+		NotificationSettings *UpdatePortalAppNotificationSettings `json:"notificationSettings,omitempty"`
+		Whitelists           *WhitelistsObject                    `json:"whitelists,omitempty"`
+	}
+
+	UpdateApplicationSettings struct {
+		ID                string      `json:"id,omitempty"`
+		Environment       Environment `json:"environment"`
+		SecretKey         string      `json:"secretKey"`
+		SecretKeyRequired *bool       `json:"secretKeyRequired"`
+		MonthlyRelayLimit int         `json:"monthlyRelayLimit"`
+		FavoritedChainIDs []string    `json:"favoritedChainIDs"`
+	}
+
+	UpdatePortalAppNotificationSettings struct {
+		ID               string           `json:"id,omitempty"`
+		NotificationType NotificationType `json:"notificationType"`
+		Active           *bool            `json:"active"`
+		Destination      string           `json:"destination"`
+		Trigger          string           `json:"trigger"`
+		Events           []string         `json:"events"`
+	}
+)
+
+// UserID returns the UserID of the Application OWNER
+func (a *PortalApp) UserID() UserID {
+	for userID, user := range a.Account.Users {
+		if user.RoleName == RoleOwner {
+			return userID
+		}
+	}
+	return ""
+}
+
+// MonthlyLimit returns the monthly relay limit for a given application
+func (a *PortalApp) MonthlyLimit() int {
+	return a.Settings.MonthlyRelayLimit
+}
+
+// IsOriginWhitelisted returns a boolean indicating whether the given ORIGIN is whitelisted for an application
+func (a *PortalApp) IsOriginWhitelisted(origin Origin) bool {
+	_, ok := a.Whitelists.Origins[origin]
+	return ok
+}
+
+// IsUserAgentWhitelisted returns a boolean indicating whether the given USER AGENT is whitelisted for an application
+func (a *PortalApp) IsUserAgentWhitelisted(userAgent UserAgent) bool {
+	_, ok := a.Whitelists.UserAgents[userAgent]
+	return ok
+}
+
+// IsBlockchainWhitelisted returns a boolean indicating whether the given BLOCKCHAIN is whitelisted for an application
+func (a *PortalApp) IsBlockchainWhitelisted(blockchain BlockchainID) bool {
+	_, ok := a.Whitelists.Blockchains[blockchain]
+	return ok
+}
+
+// IsContractWhitelisted returns a boolean indicating whether the given CONTRACT is whitelisted for a blockchain and application
+func (a *PortalApp) IsContractWhitelisted(blockchainID BlockchainID, contract Contract) bool {
+	if chainContracts, contractsOK := a.Whitelists.Contracts[blockchainID]; contractsOK {
+		if _, contractOK := chainContracts[contract]; contractOK {
+			return true
+		}
+	}
+	return false
+}
+
+// IsMethodWhitelisted returns a boolean indicating whether the given METHOD is whitelisted for a blockchain and application
+func (a *PortalApp) IsMethodWhitelisted(blockchainID BlockchainID, method Method) bool {
+	if chainMethods, methodsOK := a.Whitelists.Methods[blockchainID]; methodsOK {
+		if _, methodOK := chainMethods[method]; methodOK {
+			return true
+		}
+	}
+	return false
+}
+
+// GetWhitelistsObject returns a GraphQL-compatible WhitelistsObject struct that contains all whitelists for an application (used for Portal UI)
+func (a *PortalApp) GetWhitelistsObject() *WhitelistsObject {
+	var origins, userAgents, blockchains []string // App whitelists
+
+	for origin := range a.Whitelists.Origins {
+		origins = append(origins, string(origin))
+	}
+	sort.Strings(origins)
+
+	for userAgent := range a.Whitelists.UserAgents {
+		userAgents = append(userAgents, string(userAgent))
+	}
+	sort.Strings(userAgents)
+
+	for blockchain := range a.Whitelists.Blockchains {
+		blockchains = append(blockchains, string(blockchain))
+	}
+	sort.Strings(blockchains)
+
+	var contractWhitelists, methodWhitelists []BlockchainIDWhitelists // Chain whitelists
+
+	for blockchainID, chainContracts := range a.Whitelists.Contracts {
+		values := []string{}
+		for contract := range chainContracts {
+			values = append(values, string(contract))
+		}
+		sort.Strings(values)
+		contractWhitelists = append(contractWhitelists, BlockchainIDWhitelists{BlockchainID: string(blockchainID), Values: values})
+	}
+	sort.Slice(contractWhitelists, func(i, j int) bool {
+		return contractWhitelists[i].BlockchainID < contractWhitelists[j].BlockchainID
+	})
+
+	for blockchainID, chainMethods := range a.Whitelists.Methods {
+		values := []string{}
+		for method := range chainMethods {
+			values = append(values, string(method))
+		}
+		sort.Strings(values)
+		methodWhitelists = append(methodWhitelists, BlockchainIDWhitelists{BlockchainID: string(blockchainID), Values: values})
+	}
+	sort.Slice(methodWhitelists, func(i, j int) bool {
+		return methodWhitelists[i].BlockchainID < methodWhitelists[j].BlockchainID
+	})
+
+	return &WhitelistsObject{
+		AppWhitelists: [3]ApplicationWhitelists{
+			{Type: WLOrigins, Values: origins},
+			{Type: WLUserAgents, Values: userAgents},
+			{Type: WLBlockchains, Values: blockchains},
+		},
+		ChainWhitelists: [2]ChainWhitelists{
+			{Type: WLContracts, Values: contractWhitelists},
+			{Type: WLMethods, Values: methodWhitelists},
+		},
+	}
+}
---
+++ types/portal_app_test.go
@@ -0,0 +1,440 @@
+package types
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+var testPortalApplication = PortalApp{
+	ID:        "test_5416bb8d696386455b8",
+	Name:      "test_portal_app_123",
+	Gigastake: true,
+	Account: Account{
+		PayPlan: PayPlan{
+			Type:              FreetierV0,
+			MonthlyRelayLimit: 250_000,
+			ThroughputLimit:   2_000,
+			AppLimit:          2,
+			BlockchainIDs:     map[BlockchainID]struct{}{"0001": {}, "0056": {}, "0002": {}, "003E": {}},
+		},
+		Users: map[UserID]AccountUserAccess{
+			"user_id_123": {
+				User:     User{ID: "user_id_123", Email: "test_owner@user.com", AuthProvider: ProviderAuth0},
+				RoleName: RoleOwner,
+				Accepted: true,
+			},
+			"user_id_456": {
+				User:     User{ID: "user_id_456", Email: "test_member@user.com", AuthProvider: ProviderAuth0},
+				RoleName: RoleMember,
+				Accepted: true,
+			},
+		},
+		PartnerBlockchainIDs:   map[BlockchainID]struct{}{"0001": {}, "0056": {}, "0002": {}, "003E": {}},
+		PartnerThroughputLimit: 2_000,
+		PartnerAppLimit:        2,
+	},
+	AAT: AppAAT{
+		Address:         "test_34715cae753e67c75fbb340442e7de8e",
+		PublicKey:       "test_11b8d394ca331d7c7a71ca1896d630f6",
+		ClientPublicKey: "test_9e9ca4fe13725d412003f4bc518f6974",
+		PrivateKey:      "test_89a3af6a587aec02cfade6f5000424c2",
+		Signature:       "test_1dc39a2e5a84a35bf030969a0b3231f7",
+		Version:         "0.0.1",
+	},
+	Settings: AppSettings{
+		Environment:            EnvProduction,
+		SecretKey:              "test_90210ac4bdd3423e24877d1ff92",
+		SecretKeyRequired:      true,
+		MonthlyRelayLimit:      250_000,
+		FavoritedBlockchainIDs: map[BlockchainID]struct{}{"0001": {}, "0056": {}, "0002": {}, "003E": {}},
+	},
+	Whitelists: AppWhitelists{
+		Origins:     map[Origin]struct{}{"https://www.example.com": {}, "https://subdomain.example.com": {}, "https://portalgun.io": {}},
+		UserAgents:  map[UserAgent]struct{}{"Mozilla Firefox": {}, "Brave": {}, "Google Chrome": {}, "Safari": {}, "Internet Explorer (lol)": {}},
+		Blockchains: map[BlockchainID]struct{}{"0001": {}, "0056": {}, "0002": {}, "003E": {}},
+		Contracts: map[BlockchainID]map[Contract]struct{}{
+			"0001": {"0xtest_5fbfe3e9af3971dd833d26ba9b5c936f0be": {}, "0xtest_2f78db6436527729929aaf6c616361de0f7": {}},
+			"0056": {"0xtest_5068778dd592e39a122f4f5a5cf09c90fe2": {}, "0xtest_00000f279d81a1d3cc75430faa017fa5a2e": {}},
+			"0002": {"0xtest_1111117dc0aa78b770fa6a738034120c302": {}, "0xtest_a39b223fe8d0a0e5c4f27ead9083c756cc2": {}},
+			"003E": {"0xtest_f958d2ee523a2206206994597c13d831ec7": {}, "0xtest_0a85d5af5bf1d1762f925bdaddc4201f984": {}},
+		},
+		Methods: map[BlockchainID]map[Method]struct{}{
+			"0001": {"GET": {}, "POST": {}, "PUT": {}},
+			"0056": {"GET": {}, "POST": {}},
+			"0002": {"GET": {}, "POST": {}, "PUT": {}, "DELETE": {}},
+			"003E": {"GET": {}},
+		},
+	},
+	Notifications: map[NotificationType]AppNotification{
+		NotificationEmail: {
+			Active: true, Destination: "test@user.com", Trigger: "what_am_i", //TODO what is trigger?
+			Events: map[NotificationEvent]bool{
+				"signedUp":      true,
+				"quarter":       true,
+				"threeQuarters": true,
+			},
+		},
+		NotificationWebhook: {
+			Active: false, Destination: "https://wh.destination.io", Trigger: "what_am_i",
+			Events: map[NotificationEvent]bool{
+				"signedUp": true,
+				"full":     true,
+			},
+		},
+	},
+	CreatedAt: time.Date(2023, time.February, 14, 11, 11, 11, 0, time.UTC),
+	UpdatedAt: time.Date(2023, time.February, 27, 13, 13, 13, 0, time.UTC),
+}
+
+func Test_PortalApp_IsOriginWhitelisted(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		origin         Origin
+		expectedResult bool
+	}{
+		{
+			name:           "Should return true if a given origin is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			origin:         Origin("https://portalgun.io"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return true if a given origin is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			origin:         Origin("https://www.example.com"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return false if a given origin is not whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			origin:         Origin("https://ricksanchez.io"),
+			expectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		isOriginWhitelisted := test.portalApp.IsOriginWhitelisted(test.origin)
+		c.Equal(test.expectedResult, isOriginWhitelisted)
+	}
+}
+
+func Test_PortalApp_IsUserAgentWhitelisted(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		userAgent      UserAgent
+		expectedResult bool
+	}{
+		{
+			name:           "Should return true if a given user agent is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			userAgent:      UserAgent("Brave"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return true if a given user agent is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			userAgent:      UserAgent("Safari"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return false if a given user agent is not whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			userAgent:      UserAgent("Bird Person"),
+			expectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		isUserAgentWhitelisted := test.portalApp.IsUserAgentWhitelisted(test.userAgent)
+		c.Equal(test.expectedResult, isUserAgentWhitelisted)
+	}
+}
+
+func Test_PortalApp_IsBlockchainWhitelisted(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		blockchain     BlockchainID
+		expectedResult bool
+	}{
+		{
+			name:           "Should return true if a given blockchain is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("0001"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return true if a given blockchain is whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("003E"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return false if a given blockchain is not whitelisted for a given app",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("7009"),
+			expectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		isBlockchainWhitelisted := test.portalApp.IsBlockchainWhitelisted(test.blockchain)
+		c.Equal(test.expectedResult, isBlockchainWhitelisted)
+	}
+}
+
+func Test_PortalApp_IsContractWhitelisted(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		blockchain     BlockchainID
+		contract       Contract
+		expectedResult bool
+	}{
+		{
+			name:           "Should return true if a given contract is whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("0001"),
+			contract:       Contract("0xtest_5fbfe3e9af3971dd833d26ba9b5c936f0be"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return true if a given contract is whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("003E"),
+			contract:       Contract("0xtest_0a85d5af5bf1d1762f925bdaddc4201f984"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return false if a given contract is not whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("0056"),
+			contract:       Contract("0xtest_04938rfj439fj3409jf0439fjf4304f4444"),
+			expectedResult: false,
+		},
+		{
+			name:           "Should return false if a given contract is not whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("7009"),
+			contract:       Contract("0xtest_439834fnin3f2032f03re3j2f30fj33f3f3"),
+			expectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		isBlockchainWhitelisted := test.portalApp.IsContractWhitelisted(test.blockchain, test.contract)
+		c.Equal(test.expectedResult, isBlockchainWhitelisted)
+	}
+}
+
+func Test_PortalApp_IsMethodWhitelisted(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		blockchain     BlockchainID
+		method         Method
+		expectedResult bool
+	}{
+		{
+			name:           "Should return true if a given method is whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("0001"),
+			method:         Method("POST"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return true if a given method is whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("003E"),
+			method:         Method("GET"),
+			expectedResult: true,
+		},
+		{
+			name:           "Should return false if a given method is not whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("0056"),
+			method:         Method("PUT"),
+			expectedResult: false,
+		},
+		{
+			name:           "Should return false if a given method is not whitelisted for a given app and blockchain",
+			portalApp:      testPortalApplication,
+			blockchain:     BlockchainID("7009"),
+			method:         Method("GET"),
+			expectedResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		isBlockchainWhitelisted := test.portalApp.IsMethodWhitelisted(test.blockchain, test.method)
+		c.Equal(test.expectedResult, isBlockchainWhitelisted)
+	}
+}
+
+func Test_PortalApp_GetWhitelistsObject(t *testing.T) {
+	c := require.New(t)
+
+	tests := []struct {
+		name           string
+		portalApp      PortalApp
+		expectedResult *WhitelistsObject
+		expectedJSON   string
+	}{
+		{
+			name:      "Should return an applications whitelists in the form of a WhitelistContracts struct",
+			portalApp: testPortalApplication,
+			expectedResult: &WhitelistsObject{
+				AppWhitelists: [3]ApplicationWhitelists{
+					{Type: "origins", Values: []string{"https://portalgun.io", "https://subdomain.example.com", "https://www.example.com"}},
+					{Type: "userAgents", Values: []string{"Brave", "Google Chrome", "Internet Explorer (lol)", "Mozilla Firefox", "Safari"}},
+					{Type: "blockchains", Values: []string{"0001", "0002", "003E", "0056"}},
+				},
+				ChainWhitelists: [2]ChainWhitelists{
+					{Type: "contracts", Values: []BlockchainIDWhitelists{
+						{BlockchainID: "0001", Values: []string{"0xtest_2f78db6436527729929aaf6c616361de0f7", "0xtest_5fbfe3e9af3971dd833d26ba9b5c936f0be"}},
+						{BlockchainID: "0002", Values: []string{"0xtest_1111117dc0aa78b770fa6a738034120c302", "0xtest_a39b223fe8d0a0e5c4f27ead9083c756cc2"}},
+						{BlockchainID: "003E", Values: []string{"0xtest_0a85d5af5bf1d1762f925bdaddc4201f984", "0xtest_f958d2ee523a2206206994597c13d831ec7"}},
+						{BlockchainID: "0056", Values: []string{"0xtest_00000f279d81a1d3cc75430faa017fa5a2e", "0xtest_5068778dd592e39a122f4f5a5cf09c90fe2"}},
+					},
+					},
+					{Type: "methods", Values: []BlockchainIDWhitelists{
+						{BlockchainID: "0001", Values: []string{"GET", "POST", "PUT"}},
+						{BlockchainID: "0002", Values: []string{"DELETE", "GET", "POST", "PUT"}},
+						{BlockchainID: "003E", Values: []string{"GET"}},
+						{BlockchainID: "0056", Values: []string{"GET", "POST"}},
+					},
+					},
+				},
+			},
+			expectedJSON: `{
+				"appWhitelists": [
+					{
+						"type": "origins",
+						"values": [
+							"https://portalgun.io",
+							"https://subdomain.example.com",
+							"https://www.example.com"
+						]
+					},
+					{
+						"type": "userAgents",
+						"values": [
+							"Brave",
+							"Google Chrome",
+							"Internet Explorer (lol)",
+							"Mozilla Firefox",
+							"Safari"
+						]
+					},
+					{
+						"type": "blockchains",
+						"values": [
+							"0001",
+							"0002",
+							"003E",
+							"0056"
+						]
+					}
+				],
+				"chainWhitelists": [
+					{
+						"type": "contracts",
+						"values": [
+							{
+								"blockchainID": "0001",
+								"values": [
+									"0xtest_2f78db6436527729929aaf6c616361de0f7",
+									"0xtest_5fbfe3e9af3971dd833d26ba9b5c936f0be"
+								]
+							},
+							{
+								"blockchainID": "0002",
+								"values": [
+									"0xtest_1111117dc0aa78b770fa6a738034120c302",
+									"0xtest_a39b223fe8d0a0e5c4f27ead9083c756cc2"
+								]
+							},
+							{
+								"blockchainID": "003E",
+								"values": [
+									"0xtest_0a85d5af5bf1d1762f925bdaddc4201f984",
+									"0xtest_f958d2ee523a2206206994597c13d831ec7"
+								]
+							},
+							{
+								"blockchainID": "0056",
+								"values": [
+									"0xtest_00000f279d81a1d3cc75430faa017fa5a2e",
+									"0xtest_5068778dd592e39a122f4f5a5cf09c90fe2"
+								]
+							}
+						]
+					},
+					{
+						"type": "methods",
+						"values": [
+							{
+								"blockchainID": "0001",
+								"values": [
+									"GET",
+									"POST",
+									"PUT"
+								]
+							},
+							{
+								"blockchainID": "0002",
+								"values": [
+									"DELETE",
+									"GET",
+									"POST",
+									"PUT"
+								]
+							},
+							{
+								"blockchainID": "003E",
+								"values": [
+									"GET"
+								]
+							},
+							{
+								"blockchainID": "0056",
+								"values": [
+									"GET",
+									"POST"
+								]
+							}
+						]
+					}
+				]
+			}`,
+		},
+	}
+
+	for _, test := range tests {
+		whitelistsObject := test.portalApp.GetWhitelistsObject()
+		c.Equal(test.expectedResult, whitelistsObject)
+
+		// check that JSON is equal as well
+		resultJSON, err := json.MarshalIndent(whitelistsObject, "", "  ")
+		c.NoError(err)
+		expectedJSON := strings.ReplaceAll(strings.ReplaceAll(test.expectedJSON, " ", ""), "\t", "")
+		actualJSON := strings.ReplaceAll(strings.ReplaceAll(string(resultJSON), " ", ""), "\t", "")
+		c.Equal(expectedJSON, actualJSON)
+	}
+}
