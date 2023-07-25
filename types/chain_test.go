9a38c9c457366f5abec2d78bd7753543bc2e88ad
Pascal van Leeuwen <47662958+commoddity@users.noreply.github.com>
2023-07-25 12:42:46 -0700

    fix: add utility methods to ChainDomain struct (#134)

--- types/chain_test.go
+++ types/chain_test.go
@@ -6,6 +6,135 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_ChainDomain_IsValid(t *testing.T) {
+	tests := []struct {
+		name     string
+		domain   ChainDomain
+		expected bool
+	}{
+		{
+			name:     "Should return true for a valid domain without port",
+			domain:   ChainDomain("pokt-rpc.gateway.pokt.network"),
+			expected: true,
+		},
+		{
+			name:     "Should return true for a valid domain with port",
+			domain:   ChainDomain("pokt-rpc.gateway.pokt.network:80"),
+			expected: true,
+		},
+		{
+			name:     "Should return false for a domain without TLD",
+			domain:   ChainDomain("pokt-rpc.gateway"),
+			expected: false,
+		},
+		{
+			name:     "Should return false for an invalid domain",
+			domain:   ChainDomain("pokt-rpc..gateway.pokt.network"),
+			expected: false,
+		},
+		{
+			name:     "Should return false for an empty domain",
+			domain:   ChainDomain(""),
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isValid := test.domain.IsValid()
+			assert.Equal(t, test.expected, isValid)
+		})
+	}
+}
+
+func Test_ChainDomain_IsPublicEndpoint(t *testing.T) {
+	tests := []struct {
+		name     string
+		domain   ChainDomain
+		expected bool
+	}{
+		{
+			name:     "Should return true for public endpoint",
+			domain:   ChainDomain("pokt-rpc.gateway.pokt.network"),
+			expected: true,
+		},
+		{
+			name:     "Should return false for non-public endpoint",
+			domain:   ChainDomain("pokt.gateway.pokt.network"),
+			expected: false,
+		},
+		{
+			name:     "Should return false for empty domain",
+			domain:   ChainDomain(""),
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isPublic := test.domain.IsPublicEndpoint()
+			assert.Equal(t, test.expected, isPublic)
+		})
+	}
+}
+
+func Test_ChainDomain_IsWildcardDomain(t *testing.T) {
+	tests := []struct {
+		name     string
+		domain   ChainDomain
+		expected bool
+	}{
+		{
+			name:     "Should return true for wildcard domain",
+			domain:   ChainDomain("*.gateway.pokt.network"),
+			expected: true,
+		},
+		{
+			name:     "Should return false for non-wildcard domain",
+			domain:   ChainDomain("pokt-rpc.gateway.pokt.network"),
+			expected: false,
+		},
+		{
+			name:     "Should return false for empty domain",
+			domain:   ChainDomain(""),
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isWildcard := test.domain.IsWildcardDomain()
+			assert.Equal(t, test.expected, isWildcard)
+		})
+	}
+}
+
+func Test_ChainDomain_GetAlias(t *testing.T) {
+	tests := []struct {
+		name     string
+		domain   ChainDomain
+		expected ChainAlias
+	}{
+		{
+			name:     "Should return the alias for valid domain",
+			domain:   ChainDomain("pokt-mainnet.gateway.pokt.network"),
+			expected: ChainAlias("pokt-mainnet"),
+		},
+		{
+			name:     "Should return empty alias for empty domain",
+			domain:   ChainDomain(""),
+			expected: ChainAlias(""),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			alias := test.domain.GetAlias()
+			assert.Equal(t, test.expected, alias)
+		})
+	}
+}
+
 func Test_GetChainAliases(t *testing.T) {
 	tests := []struct {
 		name     string
