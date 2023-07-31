0379d06e927f023efc3e90663fb2a9687a590eab
Pascal van Leeuwen <pascal@pokt.network>
2023-07-31 15:05:09 -0700

    fix: add IsEVM method to Chain

--- types/chain_test.go
+++ types/chain_test.go
@@ -135,6 +135,121 @@ func Test_ChainDomain_GetAlias(t *testing.T) {
 	}
 }
 
+func Test_GetChainAltruists(t *testing.T) {
+	tests := []struct {
+		name     string
+		chain    Chain
+		expected []Altruist
+	}{
+		{
+			name: "Should return all altruists of the chain",
+			chain: Chain{
+				Altruists: map[AltruistURL]Altruist{
+					"https://fluffy-bunny.com": {
+						ChainID:  "testChainID",
+						URL:      "https://fluffy-bunny.com",
+						Auth:     "carrotAuth",
+						AuthType: ChainAuthTypeBearerToken,
+					},
+					"https://super-kitten.io": {
+						ChainID:  "testChainID",
+						URL:      "https://super-kitten.io",
+						Auth:     "meowAuth",
+						AuthType: ChainAuthTypeBearerToken,
+					},
+				},
+			},
+			expected: []Altruist{
+				{
+					ChainID:  "testChainID",
+					URL:      "https://fluffy-bunny.com",
+					Auth:     "carrotAuth",
+					AuthType: ChainAuthTypeBearerToken,
+				},
+				{
+					ChainID:  "testChainID",
+					URL:      "https://super-kitten.io",
+					Auth:     "meowAuth",
+					AuthType: ChainAuthTypeBearerToken,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			altruists := test.chain.GetChainAltruists()
+			assert.ElementsMatch(t, test.expected, altruists)
+		})
+	}
+}
+
+func Test_IsEVM(t *testing.T) {
+	tests := []struct {
+		name     string
+		chain    Chain
+		expected bool
+	}{
+		{
+			name: "Chain with EVM check",
+			chain: Chain{
+				ID: "1001",
+				Checks: map[ChainCheckType]Check{
+					ChainCheckTypeChain: {
+						ChainID:    "1001",
+						Type:       ChainCheckTypeChain,
+						Payload:    `{"method":"eth_chainId","id":10,"jsonrpc":"2.0"}`,
+						ResultKey:  "id",
+						Allowance:  10,
+						EVMChainID: 10,
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "Chain without EVM check",
+			chain: Chain{
+				ID: "2002",
+				Checks: map[ChainCheckType]Check{
+					ChainCheckTypeSync: {
+						ChainID:    "2002",
+						Type:       ChainCheckTypeSync,
+						Payload:    `{"id":20,"jsonrpc":"2.0","method":"hmy_blockNumber","params":[]}`,
+						ResultKey:  "result",
+						Allowance:  20,
+						EVMChainID: 20,
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "Chain with no checks",
+			chain: Chain{
+				ID:     "3003",
+				Checks: map[ChainCheckType]Check{},
+			},
+			expected: false,
+		},
+		{
+			name: "Chain is nil",
+			chain: Chain{
+				ID:     "4004",
+				Checks: nil,
+			},
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isEVM := test.chain.IsEVM(ChainCheckTypeChain)
+			assert.Equal(t, test.expected, isEVM)
+		})
+	}
+}
+
 func Test_GetChainAliases(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -157,6 +272,65 @@ func Test_GetChainAliases(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			aliases := test.chain.GetChainAliases()
 			assert.ElementsMatch(t, test.expected, aliases)
+		})
+	}
+}
+
+func Test_GetChainCheck(t *testing.T) {
+	chain := &Chain{
+		Checks: map[ChainCheckType]Check{
+			ChainCheckTypeChain: {
+				Type:       ChainCheckTypeChain,
+				Payload:    "test payload",
+				ResultKey:  "test key",
+				Allowance:  1000,
+				EVMChainID: 1,
+			},
+			ChainCheckTypeArchival: {
+				Type:       ChainCheckTypeArchival,
+				Payload:    "test payload",
+				ResultKey:  "test key",
+				Allowance:  1000,
+				EVMChainID: 1,
+			},
+		},
+	}
+
+	tests := []struct {
+		name      string
+		checkType ChainCheckType
+		want      Check
+	}{
+		{
+			name:      "Test getting Chain check",
+			checkType: ChainCheckTypeChain,
+			want: Check{
+				Type:       ChainCheckTypeChain,
+				Payload:    "test payload",
+				ResultKey:  "test key",
+				Allowance:  1000,
+				EVMChainID: 1,
+			},
+		},
+		{
+			name:      "Test getting Archival check",
+			checkType: ChainCheckTypeArchival,
+			want: Check{
+				Type:       ChainCheckTypeArchival,
+				Payload:    "test payload",
+				ResultKey:  "test key",
+				Allowance:  1000,
+				EVMChainID: 1,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := chain.GetChainCheck(tt.checkType)
+			if got != tt.want {
+				t.Errorf("GetChainCheck() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
@@ -212,6 +386,38 @@ func Test_GetGigastakeApps(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			aats := test.chain.GetGigastakeApps()
 			assert.ElementsMatch(t, test.expected, aats)
+		})
+	}
+}
+
+func Test_ClearGigastakeApps(t *testing.T) {
+	tests := []struct {
+		name     string
+		chain    Chain
+		expected Chain
+	}{
+		{
+			name: "Should clear all GigastakeApps of the chain",
+			chain: Chain{
+				GigastakeApps: map[GigastakeAppID]*GigastakeApp{
+					"test_app_1": {
+						ID: "test_app_1",
+					},
+					"test_app_2": {
+						ID: "test_app_2",
+					},
+				},
+			},
+			expected: Chain{
+				GigastakeApps: nil,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			clearedChain := test.chain.ClearGigastakeApps()
+			assert.Equal(t, test.expected, clearedChain)
 		})
 	}
 }
