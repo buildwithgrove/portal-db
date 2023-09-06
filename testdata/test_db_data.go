8d0b37bd7da8569d5d0bdfb296624ae9d33ba1ee
Pascal van Leeuwen <pascal@pokt.network>
2023-09-06 13:09:13 -0700

    feat: update cache to set plan type from portal app legacy fields

--- testdata/test_db_data.go
+++ testdata/test_db_data.go
@@ -803,7 +803,7 @@ var (
 				},
 			},
 			Plan: types.PlanLite{
-				PlanType:        types.PayPlanType("basic_plan"),
+				PlanType:        types.FreetierV0,
 				ChainIDs:        map[types.RelayChainID]struct{}{"0001": {}, "0053": {}},
 				ThroughputLimit: 5_000,
 			},
@@ -835,8 +835,8 @@ var (
 				},
 			},
 			Plan: types.PlanLite{
-				PlanType:        types.PayPlanType("pro_plan"),
-				ChainIDs:        map[types.RelayChainID]struct{}{"0001": {}, "0053": {}, "0021": {}, "0064": {}},
+				PlanType:        types.PayAsYouGoV0,
+				ChainIDs:        map[types.RelayChainID]struct{}{"0001": {}, "0053": {}},
 				ThroughputLimit: 10_000,
 			},
 		},
@@ -847,9 +847,9 @@ var (
 				"test_f6a5d8690ecb669865bd752b7796a920",
 			},
 			Plan: types.PlanLite{
-				PlanType:        types.PayPlanType("startup_plan"),
-				ChainIDs:        map[types.RelayChainID]struct{}{"0001": {}, "0053": {}, "0064": {}, "0034": {}},
-				ThroughputLimit: 1_000,
+				PlanType:        types.Enterprise,
+				ChainIDs:        map[types.RelayChainID]struct{}{"0001": {}, "0053": {}},
+				ThroughputLimit: 10_000,
 			},
 		},
 	}
